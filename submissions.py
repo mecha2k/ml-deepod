9fba8bf6a294932cbfe22bb9c8ce8c04cc88d454
mecha2k <mecha2k@naver.com>
2024-08-28 19:06:45 +0900

    init

--- submissions.py
+++ submissions.py
@@ -32,7 +32,7 @@ def check_graphs_v1(data, preds, threshold=None, name="default", piece=15):
     fig = plt.figure(figsize=(12, 6))
     plt.ticklabel_format(style="scientific", axis="both", scilimits=(0, 0))
     xticks = range(0, len(data))
-    plt.ylim(0, data.mean() / 2)
+    plt.ylim(0, data.mean() * 2)
     plt.xticks(np.arange(0, len(data), step=50000), rotation=45)
     plt.plot(xticks, data, color="green")
     plt.plot(xticks, preds, color="blue", linestyle="solid", linewidth=2, alpha=0.3)
@@ -43,9 +43,9 @@ def check_graphs_v1(data, preds, threshold=None, name="default", piece=15):
     fig.savefig(f"{name}_all")
 
 
-def check_graphs_v2(data, preds, anomaly, interval=10000, img_path=None, mode="train"):
+def check_graphs_v2(data, preds, anomaly, threshold, interval=10000, img_path=None, mode="train"):
     pieces = int(len(data) // interval)
-    scale = anomaly.mean() / 2
+    scale = anomaly.mean()
     for i in range(pieces):
         start = i * interval
         end = min(start + interval, len(data))
@@ -65,24 +65,29 @@ def check_graphs_v2(data, preds, anomaly, interval=10000, img_path=None, mode="t
         fig.savefig(img_path / f"{mode}_raw_data" / f"raw_{i+1:02d}_pages")
 
 
-if __name__ == "__main__":
-    data_path = Path("datasets/open")
-    with open(data_path / "test.pkl", "rb") as f:
-        test = pickle.load(f)
-    with open(data_path / "scores.pkl", "rb") as f:
-        scores = pickle.load(f)
-    scores = scores.values.flatten()
-    scores_f = signal.detrend(scores, type="linear")
-    scores_f = scores_f - scores_f.min()
-
-    anomaly_ratio = 2
+def find_anomaly(scores, anomaly_ratio=2):
     threshold = np.percentile(scores, 100 - anomaly_ratio)
     print(f"Anomaly threshold: {threshold}")
     prediction = np.zeros_like(scores)
     prediction[scores > threshold] = 1
     check_graphs_v1(scores, prediction, threshold, name="images/test_anomaly")
-    check_graphs_v2(test.values, prediction, scores, img_path=Path("images"), mode="test")
+    check_graphs_v2(
+        test.values, prediction, scores, threshold, img_path=Path("images"), mode="test"
+    )
+    return prediction
 
+
+if __name__ == "__main__":
+    data_path = Path("datasets/open")
+    with open(data_path / "test.pkl", "rb") as f:
+        test = pickle.load(f)
+    with open(data_path / "test_scores.pkl", "rb") as f:
+        scores = pickle.load(f)
+    scores = scores.values.flatten()
+    scores_f = signal.detrend(scores, type="linear")
+    scores_f = scores_f - scores_f.min()
+
+    prediction = find_anomaly(scores_f, anomaly_ratio=2)
     sample_submission = pd.read_csv(data_path / "sample_submission.csv")
     sample_submission["anomaly"] = prediction
     sample_submission.to_csv(data_path / "final_submission.csv", index=False)
--- test.py
+++ test.py
@@ -4,7 +4,7 @@ from pathlib import Path
 import pickle
 import pandas as pd
 
-data_path = Path("datasets")
+data_path = Path("datasets/open")
 train_df = pickle.load(open(data_path / "train.pkl", "rb"))
 test_df = pickle.load(open(data_path / "test.pkl", "rb"))
 X_train = train_df.values
@@ -12,10 +12,14 @@ X_test = test_df.values
 
 clf = AnomalyTransformer()
 clf.fit(X_train)
-scores = clf.decision_function(X_test)
-scores = pd.DataFrame(scores, index=test_df.index)
-scores.to_pickle(data_path / "scores.pkl")
-print(scores)
+
+train_scores = clf.decision_function(X_train)
+train_scores = pd.DataFrame(train_scores, index=train_df.index)
+train_scores.to_pickle(data_path / "train_scores.pkl")
+
+test_scores = clf.decision_function(X_test)
+test_scores = pd.DataFrame(test_scores, index=test_df.index)
+test_scores.to_pickle(data_path / "test_scores.pkl")
 
 # # evaluation of time series anomaly detection
 # from deepod.metrics import ts_metrics
