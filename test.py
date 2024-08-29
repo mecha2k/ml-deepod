30b22ccd407730ffac7ec2e05c5d393d9e3b6c58
mecha2k@naver.com <mecha2k@naver.com>
2024-08-29 14:54:37 +0900

    init

--- test.py
+++ test.py
@@ -12,23 +12,104 @@ device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 device = torch.device("mps" if torch.backends.mps.is_available() else device)
 print(f"{device} is available in torch")
 
+data_path = Path("datasets/SWaT")
+data_name = [
+    "SWaT_Dataset_Normal_v1",
+    "SWaT_Dataset_Attack_v0",
+    "SWaT_train",
+    "SWaT_train2",
+    "SWaT_raw",
+    "swat2",
+]
+
+normal_v1 = pd.read_pickle(data_path / "SWaT_Dataset_Normal_v1.pkl")
+print(normal_v1.shape)
+index = normal_v1["Unnamed: 0"][1:].values
+columns = normal_v1.iloc[0].values
+normal_v1 = normal_v1.drop("Unnamed: 0", axis=1)
+norm1 = normal_v1[1:].values
+normal_v1 = pd.DataFrame(normal_v1[1:].values, index=index, columns=columns[1:])
+print(normal_v1.shape)
+print(normal_v1.head())
+print(normal_v1.tail())
+print(normal_v1["Normal/Attack"].value_counts())
+print(normal_v1.describe())
+print("=" * 100)
+
+attack_v0 = pd.read_pickle(data_path / "SWaT_Dataset_Attack_v0.pkl")
+print(attack_v0.shape)
+index = attack_v0["Unnamed: 0"][1:].values
+columns = attack_v0.iloc[0].values
+attack_v0 = attack_v0.drop("Unnamed: 0", axis=1)
+attack_v0 = pd.DataFrame(attack_v0[1:].values, index=index, columns=columns[1:])
+print(attack_v0.shape)
+print(attack_v0.head())
+print(attack_v0.tail())
+print(attack_v0["Normal/Attack"].value_counts())
+print(attack_v0.describe())
+
+# for name in data_name:
+#     train_df = pd.read_pickle(data_path / f"{name}.pkl")
+#     print(train_df.shape)
+#     print(train_df.head())
+#     break
+
+
+# index = normal_v1["Unnamed: 0"][1:].values
+# columns = normal_v1.iloc[0].values
+# print(len(columns), columns)
+# normal_v1 = normal_v1.drop("Unnamed: 0", axis=1)
+# print(normal_v1.shape)
+# print(normal_v1.head())
+# normal_v1 = pd.DataFrame(normal_v1.values, index=index, columns=columns)
+# print(normal_v1.shape)
+# print(normal_v1.head())
+
+
+# train_df = pd.read_csv(data_path / "SWaT_train.csv")
+# print(train_df.shape)
+# print(train_df.head())
+# train_df.to_pickle(data_path / "SWaT_train.pkl")
+# train_df = pd.read_csv(data_path / "SWaT_train2.csv")
+# print(train_df.shape)
+# print(train_df.head())
+# train_df.to_pickle(data_path / "SWaT_train2.pkl")
+# train_df = pd.read_csv(data_path / "SWaT_raw.csv")
+# print(train_df.shape)
+# print(train_df.head())
+# train_df.to_pickle(data_path / "SWaT_raw.pkl")
+# train_df = pd.read_csv(data_path / "swat2.csv")
+# print(train_df.shape)
+# print(train_df.head())
+# train_df.to_pickle(data_path / "swat2.pkl")
+# train_df = pd.read_excel(data_path / "SWaT_Dataset_Normal_v1.xlsx")
+# print(train_df.shape)
+# print(train_df.tail())
+# train_df.to_pickle(data_path / "SWaT_Dataset_Normal_v1.pkl")
+# train_df = pd.read_excel(data_path / "SWaT_Dataset_Attack_v0.xlsx")
+# print(train_df.shape)
+# print(train_df.tail())
+# train_df.to_pickle(data_path / "SWaT_Dataset_Attack_v0.pkl")
+
+
 data_path = Path("datasets/open")
 train_df = pickle.load(open(data_path / "train.pkl", "rb"))
 test_df = pickle.load(open(data_path / "test.pkl", "rb"))
 X_train = train_df.values
 X_test = test_df.values
 print(X_train.shape, X_test.shape)
-
-clf = AnomalyTransformer(device=device)
-clf.fit(X_train)
-
-train_scores = clf.decision_function(X_train)
-train_scores = pd.DataFrame(train_scores, index=train_df.index)
-train_scores.to_pickle(data_path / "train_scores.pkl")
-
-test_scores = clf.decision_function(X_test)
-test_scores = pd.DataFrame(test_scores, index=test_df.index)
-test_scores.to_pickle(data_path / "test_scores.pkl")
+print(train_df.describe())
+#
+# clf = AnomalyTransformer(device=device)
+# clf.fit(X_train)
+#
+# train_scores = clf.decision_function(X_train)
+# train_scores = pd.DataFrame(train_scores, index=train_df.index)
+# train_scores.to_pickle(data_path / "train_scores.pkl")
+#
+# test_scores = clf.decision_function(X_test)
+# test_scores = pd.DataFrame(test_scores, index=test_df.index)
+# test_scores.to_pickle(data_path / "test_scores.pkl")
 
 # # evaluation of time series anomaly detection
 # from deepod.metrics import ts_metrics
