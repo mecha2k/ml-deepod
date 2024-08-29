e49ce654e12afec8fee12ed2822c598a907929bb
mecha2k <mecha2k@naver.com>
2024-08-30 02:07:57 +0900

    init

--- swat_data_analysis.py
+++ swat_data_analysis.py
@@ -4,12 +4,9 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import pickle
 import warnings
-
 from pathlib import Path
-
-from alembic.operations.toimpl import drop_column
 from sklearn.preprocessing import MinMaxScaler
-from deepod.models.time_series import AnomalyTransformer, TranAD, TimesNet
+
 
 warnings.filterwarnings("ignore")
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
@@ -101,17 +98,53 @@ def check_graphs_v1(data, labels, interval=10000, img_path=None):
         fig.savefig(img_path / f"swat_raw_data" / f"raw_{i+1:02d}_pages")
 
 
+def check_graphs_v2(data, preds, threshold=None, name="default", piece=15):
+    interval = len(data) // piece
+    plt.rcParams["font.size"] = 24
+    fig, axes = plt.subplots(piece, figsize=(20, 4 * piece))
+    for i in range(piece):
+        start = i * interval
+        end = min(start + interval, len(data))
+        xticks = range(start, end)
+        axes[i].ticklabel_format(style="scientific", axis="both", scilimits=(0, 0))
+        axes[i].set_xticks(np.arange(start, end, step=5000))
+        axes[i].set_ylim(0, threshold * 2)
+        axes[i].plot(xticks, data[start:end], color="g")
+        axes[i].plot(xticks, preds[start:end], color="b")
+        if threshold is not None:
+            axes[i].axhline(y=threshold, color="r")
+        axes[i].grid()
+    plt.tight_layout()
+    fig.savefig(name)
+
+    plt.rcParams["font.size"] = 16
+    fig = plt.figure(figsize=(12, 6))
+    plt.ticklabel_format(style="scientific", axis="both", scilimits=(0, 0))
+    xticks = range(0, len(data))
+    plt.ylabel("Anomaly Score")
+    plt.ylim(0, threshold * 2)
+    plt.xticks(np.arange(0, len(data), step=50000), rotation=45)
+    plt.plot(xticks, data_f, color="g")
+    plt.plot(xticks, data, color="cyan", linewidth=2, alpha=0.3)
+    plt.plot(xticks, preds, color="b", linestyle="solid", linewidth=2, alpha=0.3)
+    if threshold is not None:
+        plt.axhline(y=threshold, color="r", linewidth=2)
+    plt.grid()
+    plt.legend(["Anomaly Score (detrend)", "Anomaly Score", "Prediction", "Threshold"])
+    plt.tight_layout()
+    fig.savefig(f"{name}_all")
+
+
 if __name__ == "__main__":
     data_path = Path("datasets/SWaT")
-    data_name = [
-        "SWaT_Dataset_Normal_v1.xlsx",
-        "SWaT_Dataset_Attack_v0.xlsx",
-        "SWaT_train.csv",
-        "SWaT_train2.csv",
-        "SWaT_raw.csv",
-        "swat2.csv",
-    ]
-
+    # data_name = [
+    #     "SWaT_Dataset_Normal_v1.xlsx",
+    #     "SWaT_Dataset_Attack_v0.xlsx",
+    #     "SWaT_train.csv",
+    #     "SWaT_train2.csv",
+    #     "SWaT_raw.csv",
+    #     "swat2.csv",
+    # ]
     # make_swat_pickle(data_name, data_path)
     # make_train_test_data(data_path)
 
@@ -122,13 +155,6 @@ if __name__ == "__main__":
     with open(data_path / "test_labels.npy", "rb") as f:
         y_test = np.load(f, allow_pickle=True)
     print(f"Shape of Train : {X_train.shape}, Test : {X_test.shape}, Labels : {y_test.shape}")
-
     check_graphs_v1(X_test, y_test, img_path=Path("images"))
 
-    # data_path = Path("datasets/open")
-    # train_df = pickle.load(open(data_path / "train.pkl", "rb"))
-    # test_df = pickle.load(open(data_path / "test.pkl", "rb"))
-    # X_train = train_df.values
-    # X_test = test_df.values
-    # print(X_train.shape, X_test.shape)
-    # print(train_df.describe())
+    scores = pd.read_pickle(data_path / "test_scores.pkl")
--- test.py
+++ test.py
@@ -5,11 +5,8 @@ import pickle
 import warnings
 
 from pathlib import Path
-
-from menuinst.utils import data_path
-
 from deepod.models.time_series import AnomalyTransformer, TranAD, TimesNet
-from testbed.testbed_unsupervised_ad import y_test
+
 
 warnings.filterwarnings("ignore")
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
@@ -27,8 +24,7 @@ def load_datasets(data_path, mode="open"):
     elif mode == "swat":
         X_train = np.load(data_path / "train.npy")
         X_test = np.load(data_path / "test.npy")
-        y_test = np.load(data_path / "test_label.npy")
-    print(X_train.shape, X_test.shape, y_test.shape)
+        y_test = np.load(data_path / "test_labels.npy")
     return X_train, X_test, y_test
 
 
@@ -42,28 +38,37 @@ if __name__ == "__main__":
     data_path = Path("datasets/SWaT")
 
     X_train, X_test, y_test = load_datasets(data_path, mode=mode)
+    print(X_train.shape, X_test.shape, y_test.shape)
 
-    clf = AnomalyTransformer(device=device)
-    clf.fit(X_train)
+    # clf = AnomalyTransformer(device=device)
+    # clf.fit(X_train)
+    #
+    # scores = clf.decision_function(X_test)
+    # print(scores.shape)
+    # print(scores[:50])
+    # scores = pd.DataFrame(scores)
+    # scores.to_pickle(data_path / "test_scores.pkl")
 
-    train_scores = clf.decision_function(X_train)
-    train_scores = pd.DataFrame(train_scores)
-    train_scores.to_pickle(data_path / "train_scores.pkl")
+    scores = pd.read_pickle(data_path / "test_scores.pkl")
+    print(scores.describe())
+    print(scores.info())
+    print(scores.head())
+    print(scores.shape)
 
-    test_scores = clf.decision_function(X_test)
-    test_scores = pd.DataFrame(test_scores)
-    test_scores.to_pickle(data_path / "test_scores.pkl")
-
-    # evaluation of time series anomaly detection
-    from deepod.metrics import ts_metrics
-    from deepod.metrics import (
-        point_adjustment,
-    )  # execute point adjustment for time series ad
-
-    scores = clf.decision_function(test)
-    eval_metrics = ts_metrics(labels, scores)
-    adj_eval_metrics = ts_metrics(labels, point_adjustment(labels, scores))
-    print(eval_metrics)
-    print(adj_eval_metrics)
-    # results : roc_auc_score, average_precision_score, best_f1, best_p, best_r
-    # (0.76625, 0.26313, 0.30726, 0.42068, 0.24201)
+    # # evaluation of time series anomaly detection
+    # from deepod.metrics import ts_metrics
+    # from deepod.metrics import (
+    #     point_adjustment,
+    # )  # execute point adjustment for time series ad
+    #
+    # scores = pd.read_pickle(data_path / "test_scores.pkl")
+    # print(scores.describe())
+    # scores = scores.values.flatten()
+    # print(scores.shape)
+    #
+    # # eval_metrics = ts_metrics(y_test, scores)
+    # adj_eval_metrics = ts_metrics(y_test, point_adjustment(y_test, scores))
+    # print(eval_metrics)
+    # print(adj_eval_metrics)
+    # # results : roc_auc_score, average_precision_score, best_f1, best_p, best_r
+    # # (0.76625, 0.26313, 0.30726, 0.42068, 0.24201)
